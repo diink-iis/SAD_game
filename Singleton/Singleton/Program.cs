a5fb7e51fd082c5ab955b677f47380021728bfb3
efimov77 <71399829+efimov77@users.noreply.github.com>
2023-03-02 17:59:43 +0300

    Homework№2

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SingletonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Singleton s1 = Singleton.Instance;
+
+            Console.WriteLine("Object: {0}", s1.GetHashCode());
+
+            Singleton s2 = Singleton.Instance;
+
+            Console.WriteLine("Object: {0}", s2.GetHashCode());
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton? instance = null;
+        private static readonly object padlock = new object();
+
+        Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
