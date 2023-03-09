6f0db36ab1f50394174c22e7b06015fdcecfa850
efimov77 <71399829+efimov77@users.noreply.github.com>
2023-03-09 22:31:59 +0300

    Lazy_Initialization

---
+++ Lazy_Initialization/Lazy_Initialization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LazyInitialization { 
+    class Program { 
+        static void Main(string[] args) { 
+            LazyObject lazyObject = new LazyObject();
+
+            Console.WriteLine($"Объект инициализирован: {lazyObject.IsInitialized}"); 
+            Console.WriteLine($"Значение свойства Value: {lazyObject.Value}"); 
+            Console.WriteLine($"Объект инициализирован: {lazyObject.IsInitialized}");
+            Console.ReadLine(); } 
+    } 
+    public class LazyObject { 
+        private int _value; 
+        private bool _isInitialized; 
+        public int Value { get { if (!_isInitialized) { Initialize(); } return _value; } } 
+        public bool IsInitialized { get { return _isInitialized; } } 
+        private void Initialize() { 
+            _value = 1; 
+            _isInitialized = true; 
+        } 
+    } 
+}
